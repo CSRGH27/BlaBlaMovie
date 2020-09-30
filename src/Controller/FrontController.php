9a304a8f318d08394ed21f8ccd9f5b7cc3878774
CSRGH27 <paulcsrgh@gmail.com>
2020-09-30 16:44:41 +0200

    install Encore bundle

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class FrontController extends AbstractController
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+
+    /**
+     * Get movie with bundle httpClient and omdbApi
+     *
+     * @Route("/" , name="app")
+     * @return void
+     */
+    public function getMovies()
+    {
+        $apiKeys = $this->getParameter('app.api_omdb_key');
+        $baseUri = "http://www.omdbapi.com/";
+        $response = $this->client->request('GET', $baseUri . '?apikey=' . $apiKeys . '&type=movie&s=digital&school');
+        dump($response->getContent());
+
+
+
+        return $this->render(
+            'app/index.html.twig',
+            ['test' => $apiKeys]
+        );
+    }
+}
